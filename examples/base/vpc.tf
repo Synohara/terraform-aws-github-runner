49868fcd1eceacd30c25a531d50204e58566bee3
Shinohara Makoto <mk-shinohara@kddi.com>
2023-01-27 12:34:17 +0900

    add private and public subnet

--- examples/base/vpc.tf
+++ examples/base/vpc.tf
@@ -6,8 +6,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}c", "${var.aws_region}d"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_dns_hostnames    = true
   enable_nat_gateway      = true
