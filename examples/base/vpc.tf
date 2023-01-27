16cc93330abae4acdd91db820063e2abdaf87897
Shinohara Makoto <mk-shinohara@kddi.com>
2023-01-27 10:29:11 +0900

    change base module vpc settings

--- examples/base/vpc.tf
+++ examples/base/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "${var.prefix}-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${var.aws_region}a", "${var.aws_region}c"]
+  azs             = ["${var.aws_region}a", "${var.aws_region}c", "${var.aws_region}d"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
